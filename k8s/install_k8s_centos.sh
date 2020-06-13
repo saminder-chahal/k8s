a14cacf41eec030f9865907a3980e408e4f4d8b9
Chahal, Saminderjit <chahalsaminder13@gmail.com>
2020-06-13 23:00:49 +0000

    Fixing bugs for installation

--- k8s/install_k8s_centos.sh
+++ k8s/install_k8s_centos.sh
@@ -1,6 +1,6 @@
 # URL: https://phoenixnap.com/kb/how-to-install-kubernetes-on-centos
 
-EOF > /etc/yum.repos.d/kubernetes.repo
+cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
 baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
@@ -10,12 +10,26 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 
+echo "======================================="
+echo "======================================="
 echo "Installing Kubelet Kubeadm and Kubectl"
 yum install -y kubelet kubeadm kubectl
+echo "======================================="
+echo "======================================="
 echo "Enabling Kubelet"
 systemctl enable kubelet
 systemctl start kubelet
 
+
+echo "======================================="
+echo "======================================="
+echo "Install Firewalld"
+sudo yum install -y firewalld
+systemctl enable firewalld
+systemctl start firewalld
+
+echo "======================================="
+echo "======================================="
 ## on Master NODE; add following ports to firewalld
 sudo firewall-cmd --permanent --add-port=6443/tcp
 sudo firewall-cmd --permanent --add-port=2379-2380/tcp
@@ -31,6 +45,8 @@ sudo firewall-cmd --reload
 # firewall-cmd –-reload
 
 
+echo "======================================="
+echo "======================================="
 # Update Iptable Settings
 echo "Updating Iptable Setttings"
 cat  < /etc/sysctl.d/master_node_name
@@ -40,6 +56,8 @@ EOF
 sysctl --system
 
 
+echo "======================================="
+echo "======================================="
 echo "Disable SELINUX"
 sudo sed -i '/swap/d' /etc/fstab
 sudo swapoff -a
--- k8s/setup_kub_master.sh
+++ k8s/setup_kub_master.sh
@@ -2,14 +2,18 @@
 
 echo "Using flanner virtual network for the pods communication"
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
-
+echo "========================================================"
+echo "========================================================"
 echo "Creating the Cluster"
 sudo kubeadm init –pod-network-cidr=10.244.0.0/16
 
+echo "========================================================"
+echo "========================================================"
 echo "Checking status of cluster"
 sudo kubectl get pods --all-namespaces
 
 
+echo "========================================================"
 ## Manager Cluster as regular user settings
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
