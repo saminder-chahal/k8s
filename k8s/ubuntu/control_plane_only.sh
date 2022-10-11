d030414ce545a57208bbd90fe775328856482a9f
saminderjit chahal <chahalsaminder13@gmail.com>
2022-10-11 06:00:24 +0000

    adding k8s master node

--- k8s/ubuntu/control_plane_only.sh
+++ k8s/ubuntu/control_plane_only.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.21.0
 
+echo "\n\n\n\n"
+echo "following other recommendations"
+mkdir -o $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
