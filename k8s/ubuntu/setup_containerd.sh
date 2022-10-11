d42f7be0bb3e95766fd9e22e378384ecc3b628ac
saminderjit chahal <chahalsaminder13@gmail.com>
2022-10-11 05:46:30 +0000

    adding ubuntu k8s installation steps

---
+++ k8s/ubuntu/setup_containerd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "instructions url: https://kubernetes.io/docs/setup/production-environment/container-runtimes/#containerd"
+sudo apt-get update && sudo apt-get install -y containerd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+echo "disable swap for stable k8s cluster"
+sudo swapoff -a 
+sudo -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
